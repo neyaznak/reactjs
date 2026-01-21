578bce9b95cc47486eb68dc0aab9d08acd837249
neyaznak <neyaznak@gmail.com>
2026-01-22 03:01:48 +0530

    chore: initialize npm project and configure Parcel for dev and build

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-/***
- * <div id="parent">
- *    <div id="child">
- *         <h1>I am an h1 tag</h1>
- *         <h2>I am an h2 tag</h2>
- *     </div>
- * <div id="child">
- *         <h1>I am an h1 tag</h1>
- *         <h2>I am an h2 tag</h2>
- *     </div>
- * </div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is React practice"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
---
+++ README.md
@@ -0,0 +1,20 @@
+# React Basics Practice Project
+
+# Parcel
+ - Dev Build
+ - Local Server
+ - HMR = Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Caching and hence fater builds (in .parcel-cache file)
+ - Image Optimization 
+ - Minification
+ - Bundling
+ - Compress
+ - Consistent hashing
+ - Code Splitting
+ - Differential Bundling - supports older browsers
+ - Diagnostics
+ - Error handling
+ - HTTPs support by parcel
+ - Tree shaking - as removes unused code
+ - Different dev and prod bundles
