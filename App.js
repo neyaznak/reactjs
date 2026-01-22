f44c4549575364743748bd9f4a17db8508b19a6a
neyaznak <neyaznak@gmail.com>
2026-01-22 11:15:16 +0530

    Functional React Component

--- App.js
+++ App.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //JSX - HTML-like or XML-like syntax
-const jsxHeading = (
-  <h1 className="head" tabIndex="1">
-    React using JSX!!!
+//React Element
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    React Title Component!
   </h1>
 );
+
+// React Component
+// Class Based Component - OLD
+// Funtional Component - NEW
+
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">React Functional Component!</h1>
+  </div>
+);
+
+//we can write like this also without return and {}
+//const HeadingComponentSingleLine = () => <h1>React Functional Component!</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+root.render(<HeadingComponent />);
