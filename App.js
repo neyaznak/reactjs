29b4a87721693e7e140b9f97049273fd129b9f19
neyaznak <neyaznak@gmail.com>
2026-01-22 11:51:24 +0530

    Different ways to render an element and any JS

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX - HTML-like or XML-like syntax
-//React Element
+const elem = <span>React Child Element</span>;
+
 const Title = () => (
   <h1 className="head" tabIndex="5">
-    React Title Component!
+    React Title Element!
   </h1>
 );
-
-// React Component
-// Class Based Component - OLD
-// Funtional Component - NEW
-
-// React Functional Component
+const number =12848624;
 const HeadingComponent = () => (
   <div id="container">
+    {Title()}
+    {number}
     <Title />
+    <Title></Title>
     <h1 className="heading">React Functional Component!</h1>
   </div>
 );
-
-//we can write like this also without return and {}
-//const HeadingComponentSingleLine = () => <h1>React Functional Component!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
